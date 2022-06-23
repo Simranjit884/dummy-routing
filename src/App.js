7cd7e0df13a713c42dc62189ea348a37b95d83f6
samy884 <ssgill884@gmail.com>
2022-06-23 19:46:15 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { Route } from "react-router-dom";
+import Product from "./components/Products";
+import Welcome from "./components/Welcome";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Product />
+      </Route>
     </div>
   );
 }
